8794c0b3baedf8b3a38c682009f8d0c71016b5ad
Juphex <20795522+Juphex@users.noreply.github.com>
2021-08-06 11:27:49 +0200

    update doc, cleanup

--- main.py
+++ main.py
@@ -16,17 +16,20 @@ if is_ipython:
 plt.ion()
 
 # from agent import Agent
-from simple_agent import Agent
+from agent import Agent
 
 FLAGS = flags.FLAGS
 
 # flags info: https://github.com/deepmind/pysc2/blob/master/pysc2/bin/agent.py
 flags.DEFINE_string("map", "MoveToBeacon", "Name of a map to use.")
+
+# Mineral Shards Map
 #flags.DEFINE_string("map", "CollectMineralShards", "Name of a map to use.")
 flags.DEFINE_integer("max_agent_steps", 0, "Total agent steps.")
 flags.DEFINE_integer("game_steps_per_episode", None, "Game steps per episode.")
 flags.DEFINE_integer("max_episodes", 0, "Total episodes.")
-# 2,5e-11, 2,5e-10, 2,5e-9, 2,5e-8, 2,5e-7 2,5e-6 2,5e-5, 2,5e-4
+
+# set learning rate, e.g.: 2,5e-11, 2,5e-10, 2,5e-9, 2,5e-8, 2,5e-7 2,5e-6 2,5e-5, 2,5e-4
 flags.DEFINE_float("learning_rate", 2e-05, "Learning rate.")
 flags.DEFINE_string("path", "model_weights.pt", "Path and filename of the network weights to be loaded and saved.")
 
@@ -34,12 +37,12 @@ flags.DEFINE_string("path", "model_weights.pt", "Path and filename of the networ
 def main(argv):
     print('non-flag arguments:', argv)
 
+    # set parameters
     lr = FLAGS.learning_rate
     path = FLAGS.path
     score_multiplier = 5
 
-    # for lr in [1e-1, 1e-2, 1e-3,  1e-4, 1e-9, 1e-10, 1e-11, 1e-12, 1e-14]:
-    #     path = "{}.pt".format(lr)
+    # start environment and agent
     with sc2_env.SC2Env(
             map_name=FLAGS.map,
             players=[sc2_env.Agent(race=sc2_env.Race.terran)],
@@ -53,7 +56,7 @@ def main(argv):
             visualize=True,
             score_multiplier=score_multiplier
     ) as env:
-        # list of players.
+        # list of agents.
         agents = [
             Agent(batch_size=16, exploration_steps=10000,
                   replay_memory_amount=10000, gamma=0.99, learning_rate=lr,
